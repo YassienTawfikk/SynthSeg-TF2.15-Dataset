a7ef403900464ea5f5af67d6ff0c98d1ddb4a62e
Gabriele Lozupone <54721912+GabrieleLozupone@users.noreply.github.com>
2025-09-27 12:18:49 +0200

    Update Python version requirement and package URL

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ import setuptools
 
 python_version = sys.version[:3]
 
-if (python_version != '3.6') & (python_version != '3.8'):
-    raise Exception('Setup.py only works with python version 3.6 or 3.8, not {}'.format(python_version))
+if (python_version != '3.11'):
+    raise Exception('Setup.py only works with python version 3.11, not {}'.format(python_version))
 
 else:
 
-    with open('requirements_python' + python_version + '.txt') as f:
+    with open('requirements'.txt') as f:
         required_packages = [line.strip() for line in f.readlines()]
 
     print(setuptools.find_packages())
@@ -20,9 +20,9 @@ else:
                      license='Apache 2.0',
                      description='Domain-agnostic segmentation of brain scans',
                      author='Benjamin Billot',
-                     url='https://github.com/BBillot/SynthSeg',
+                     url='https://github.com/GabrieleLozupone/SynthSeg-TF2.15-Dataset',
                      keywords=['segmentation', 'domain-agnostic', 'brain'],
                      packages=setuptools.find_packages(),
-                     python_requires='>=3.6',
+                     python_requires='>=3.11',
                      install_requires=required_packages,
                      include_package_data=True)
